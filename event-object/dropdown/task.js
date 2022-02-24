22dfd348ffa14f5beafe70e885722ab87f2dd7fd
artur <i@aderbin.ru>
2022-02-24 21:20:18 +0500

    fix event-object/dropdown

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,25 +1,34 @@
-const dropdown = document.querySelector('.dropdown');
-const dropValue = dropdown.querySelector('.dropdown__value');
-const dropList = dropdown.querySelector('.dropdown__list');
-const links = dropdown.querySelectorAll('.dropdown__link');
+const dropdownList = document.querySelectorAll('.dropdown');
 
 
 function clickDropValue(event) {
+    const parent = this.closest(".dropdown");
+    const dropList = parent.querySelector('.dropdown__list');
+
     dropList.classList.toggle('dropdown__list_active')
 }
 
 function selectNewValue(event) {
     event.preventDefault();
+
+    const parent = this.closest(".dropdown");
+    const dropValue = parent.querySelector('.dropdown__value');
+    const dropList = parent.querySelector('.dropdown__list');
+
     dropValue.textContent = event.target.textContent;
     dropList.classList.toggle('dropdown__list_active')
 }
 
 
 function main() {
-    dropValue.addEventListener('click', clickDropValue);
+    for(let dropdown of dropdownList) {
+        const dropValue = dropdown.querySelector('.dropdown__value');
+        dropValue.addEventListener('click', clickDropValue);
 
-    for(let el of links) {
-        el.addEventListener('click', selectNewValue);
+        const links = dropdown.querySelectorAll('.dropdown__link');
+        for(let el of links) {
+            el.addEventListener('click', selectNewValue);
+        }
     }
 }
 
