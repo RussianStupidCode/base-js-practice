76d97dbaa8b82ebb5302dd6c5a3d1aafa9a6a83b
artur <i@aderbin.ru>
2022-03-16 19:47:52 +0500

    finish hw document-structure

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,5 +1,6 @@
 const cartHTML = document.querySelector(".cart");
 const productsListHTML = document.querySelectorAll(".product");
+const storage = window.localStorage;
 
 
 class ElementTransit {
@@ -51,13 +52,14 @@ class ElementTransit {
 }
 
 class CartProduct {
-    constructor(product) {
+    constructor(product, count, index) {
+        this.index = index; // чтобы отслеживать порядок добавления при загрузке
         this.productHTML = document.createElement("div");
         this.productHTML.classList.add("cart__product");
         this.productHTML.dataset["id"] = product.id;
         this.productHTML.innerHTML = `
             <img class="cart__product-image" src="${product.imageSrc}">
-            <div class="cart__product-count">${product.count}</div>`
+            <div class="cart__product-count">${count}</div>`
         
         this.countHTML = this.productHTML.querySelector(".cart__product-count");
         this.imageHTML = this.productHTML.querySelector(".cart__product-image");
@@ -84,8 +86,12 @@ class CartProduct {
         this.countHTML.textContent = value;
     }
 
-    addProduct(product) {
-        this.count = this.count + product.count;
+    addProduct(count) {
+        this.count = this.count + count;
+    }
+
+    removeProduct(count) {
+        this.count = this.count - count;
     }
 
     remove() {
@@ -96,22 +102,81 @@ class CartProduct {
 
 class Cart {
     constructor(cartHTML) {
+        this.currentIndex = 0; // чтобы отслеживать порядок добавления при загрузке
         this.cartHTML = cartHTML;
         this.cartProductHTML = cartHTML.querySelector(".cart__products");
         this.products = {};
+        this.changeDisplay();
     }
 
-    addProduct(product) {
-        if(this.products[product.id]) {
-            this.products[product.id].addProduct(product);
-        } else {
-            this.products[product.id] = new CartProduct(product);
-            this.cartProductHTML.insertAdjacentElement("beforeEnd", this.products[product.id].HTML);
+    saveStorage(storage) {
+        let cartProducts = [];
+        for(const [key, el] of Object.entries(this.products)) {
+            const state = {
+                id: key,
+                index: el.index,
+                count: el.count,
+            };
+            cartProducts.push(state);
         }
+
+        cartProducts = cartProducts.sort((a, b) => a.index - b.index);
+        storage.setItem("cartProducts", JSON.stringify(cartProducts));
+    }
+
+    loadStorage(storage, products) {
+        let cartProducts = storage.getItem("cartProducts");
+        if(!cartProducts) {
+            return;
+        }
+        cartProducts = JSON.parse(cartProducts);
+
+        for(const cartProduct of cartProducts) {
+            if(!products[cartProduct.id]) {
+                continue;
+            }
+
+            this.addProduct(products[cartProduct.id], cartProduct.count);
+        }
+        this.changeDisplay();
+    }
+
+    changeDisplay() {
+        if(Object.keys(this.products).length === 0) {
+            this.cartHTML.classList.add("hiden");
+        } else {
+            this.cartHTML.classList.remove("hiden");
+        }
+    }
+
+    addProduct(product, count) {
+        if(this.products[product.id]) {
+            this.products[product.id].addProduct(count);
+        } else {
+            this.products[product.id] = new CartProduct(product, count, this.currentIndex++);
+            this.cartProductHTML.insertAdjacentElement("beforeEnd", this.products[product.id].HTML);
+        } 
+        this.changeDisplay();
 
         const cartProduct = this.products[product.id];
 
         ElementTransit.shadowTransition(product.imageHTML, cartProduct.imagePosition, 10, 0.1);
+    }
+
+    removeProduct(product, count) {
+        if(!this.products[product.id]) {
+            return;
+        }
+        const cartProduct = this.products[product.id];
+
+        if(cartProduct.count <= count) {
+            cartProduct.remove();
+            delete this.products[product.id];
+        } else {
+            cartProduct.removeProduct(count);
+        }
+
+        this.changeDisplay();
     }
 }
 
@@ -169,18 +234,33 @@ class Product {
 
     addCartEvent(cart) {
         const addButton = this._productHTML.querySelector(".product__add");
+        const removeButton = this._productHTML.querySelector(".product__remove");
 
         addButton.addEventListener("click", event => {
-            cart.addProduct(this);
+            cart.addProduct(this, this.count);
+            cart.saveStorage(storage);
+        });
+
+        removeButton.addEventListener("click", event => {
+            cart.removeProduct(this, this.count);
+            cart.saveStorage(storage);
         });
     }
 };
 
-const cart = new Cart(cartHTML);
-const productList = [];
 
+
+
+
+const products = {};
 for(const productHTML of productsListHTML) {
     const product = new Product(productHTML)
-    product.addCartEvent(cart);
-    productList.push(product);
+    products[product.id] = product;
+}
+
+const cart = new Cart(cartHTML);
+cart.loadStorage(storage, products)
+
+for(const [key, el] of Object.entries(products)) {
+    el.addCartEvent(cart);
 }
